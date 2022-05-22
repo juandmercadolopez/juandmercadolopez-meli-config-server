013890b7ca43cc20ac9fdaecbb2c29b539389e84
Juan David Mercado Lopez <juandmercadolopez@gmail.com>
2022-05-22 14:55:56 -0500

    creando config server

---
+++ README.md
@@ -0,0 +1,1 @@
+# juandmercadolopez-meli-backend-config-server
---
+++ src/test/java/com/juandmercadlopez/meli/config/server/CloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juandmercadlopez.meli.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
